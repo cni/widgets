568cd9c461cf2f23604e229658d5d04cdf9bcb47
Joe Sommer <jsommer1@stanford.edu>
2017-08-24 15:06:45 -0700

    Update Save_Serial_Pi.py

--- raspberrypi3/Save_Serial_Pi.py
+++ raspberrypi3/Save_Serial_Pi.py
@@ -8,7 +8,7 @@
 # the data to the checksum value in order to check for data corruption. 
 #       The program prompts the user to put in a time limit, then stops reading data after the time limit has passed. 
 
-#       See the hardware setup section of README.txt to see how to connect the Raspberry Pi 3 GPIO pins to the serial output. 
+#       See README.txt to see how to connect the Raspberry Pi 3 GPIO pins to the serial output. 
 # Joe Sommer, 2017 
 
 
